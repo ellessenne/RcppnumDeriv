952775846e3dd22bee0b8d05e40bf0711d99e9fb
Alessandro Gasparini <ellessenne@gmail.com>
2020-03-02 22:02:25 +0100

    File with package description

---
+++ R/RcppnumDeriv-package.R
@@ -0,0 +1,10 @@
+#' @title Accurate and Fast Numerical Derivatives
+#'
+#' @description Description goes here...
+#'
+#' @name RcppnumDeriv
+#' @docType package
+#' @author Alessandro Gasparini (alessandro.gasparini@@ki.se)
+#' @useDynLib RcppnumDeriv
+#' @importFrom Rcpp evalCpp
+NULL
