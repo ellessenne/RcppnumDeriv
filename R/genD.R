9cfcacf30e89766208aba47bca99cfa7eb6a578d
Alessandro Gasparini <ellessenne@gmail.com>
2020-03-18 19:53:10 +0100

    Added some documentation

--- R/genD.R
+++ R/genD.R
@@ -1,4 +1,21 @@
+#' @title Generate Bates and Watts D Matrix
+#'
+#' @description Generate a matrix of function derivative information.
+#'
+#' @param func A function whose first (vector) argument is a parameter vector;
+#' @param x The parameter vector passed to `func`;
+#' @param method.args Arguments of the algorithm, used for the approximation algorithm;
+#' @param ... Additional arguments passed to `func`.
+#'
+#' @return A list of...
+#'
+#' @details The derivatives are calculated numerically using Richardson improvement...
 #' @export
+#'
+#' @examples
+#' func <- function(x) c(x[1], x[1], x[2]^2)
+#' xxx <- c(2, 2, 5)
+#' RcppnumDeriv::genD(func, xxx)
 genD <- function(func, x, method.args = list(), ...) {
   # Parse default arguments
   args <- list(eps = 1e-4, d = 0.0001, zero.tol = sqrt(.Machine$double.eps / 7e-7), r = 4, v = 2) # default
